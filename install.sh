f24d7887b48996b955781c1d46846529528e3d4f
alexesba <alexesba@gmail.com>
2016-10-04 12:18:08 -0500

    install plugin after install

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+brew install neovim/neovim/neovim
+
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +BundleInstall +qall
