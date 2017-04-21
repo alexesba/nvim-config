57d0d5b6fc5c417b38b02bbf1a1a94244e8006ae
alexesba <alexesba@gmail.com>
2017-04-21 16:14:32 -0500

    Add command to link the bash profile file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 brew install neovim/neovim/neovim
 
+ln -s ~/.config/bash_profile ~/.bash_profile
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +BundleInstall +qall
