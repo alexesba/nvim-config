d52b317059705724793188c2ac18eeb0722b02ab
alexesba <alexesba@gmail.com>
2017-04-21 16:53:58 -0500

    make a symbolic link for tmux config file

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 brew install neovim/neovim/neovim
 
-ln -s ~/.config/bash_profile ~/.bash_profile
+ln -s ~/.config/nvim/bash_profile ~/.bash_profile
+ln -s ~/.config/nvim/tmux.conf ~/.tmux.conf
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +BundleInstall +qall
