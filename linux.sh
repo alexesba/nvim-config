3fe403820da5ab41f225553c75b3b591a6e3eec2
alexesba <alexesba@gmail.com>
2019-04-09 18:46:02 -0500

    Allow autocomplete using git aliasses

--- linux.sh
+++ linux.sh
@@ -7,6 +7,13 @@ if ! shopt -oq posix; then
   elif [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
   fi
+  __git_complete ga _git_add
+  __git_complete gap _git_add
+  __git_complete gb _git_branch
+  __git_complete gco _git_checkout
+  __git_complete gd _git_diff
+  __git_complete gdc _git_diff
+  __git_complete gs _git_status
 fi
 
 if [ -d "$HOME/.nvm" ]; then
