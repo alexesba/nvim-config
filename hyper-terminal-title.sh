f1130e9aa2a7ff928541594b82058aa49128bdcc
alexesba <alexesba@gmail.com>
2019-05-01 20:29:57 -0500

    Remove the path from the bash title

--- hyper-terminal-title.sh
+++ hyper-terminal-title.sh
@@ -1,20 +1,21 @@
 if which hyper > /dev/null; then
   case "$TERM" in
-  xterm*|rxvt*)
+    xterm*|rxvt*)
       PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
       show_command_in_title_bar()
       {
-          case "$BASH_COMMAND" in
-              *\033]0*)
+        case "$BASH_COMMAND" in
+          *\033]0*)
+            ;;
+          *)
+            read -r first _ <<< $BASH_COMMAND
+                  echo -ne "\033]0;$first - ${PWD##*/}\007"
                   ;;
-              *)
-                  echo -ne "\033]0;${BASH_COMMAND} - ${PWD##*/}\007"
-                  ;;
-          esac
-      }
+              esac
+            }
       trap show_command_in_title_bar DEBUG
       ;;
-  *)
+    *)
       ;;
   esac
 fi
