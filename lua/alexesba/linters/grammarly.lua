95e1145169f0a1cf6ce3f0af9921334c5e4b05f2
alexesba <alexesba@gmail.com>
2023-08-01 16:37:52 -0600

    Fix grammarly lsp config

--- lua/alexesba/linters/grammarly.lua
+++ lua/alexesba/linters/grammarly.lua
@@ -2,5 +2,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('lspconfig').grammarly.setup({
   on_attach = OnAttach,
-  capabilities = capabilities
+  capabilities = capabilities,
+  filetypes = { "markdown", "text", "md" },
+  init_options = {
+    clientId = 'client_BaDkMgx4X19X9UxxYRCXZo',
+  },
 })
