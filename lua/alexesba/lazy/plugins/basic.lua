ec756773a72944a6d2dd2dd6ee543a6dbf877826
alexesba <alexesba@gmail.com>
2023-11-10 15:21:13 -0600

    Order basic plugins alphabetically

--- lua/alexesba/lazy/plugins/basic.lua
+++ lua/alexesba/lazy/plugins/basic.lua
@@ -1,32 +1,4 @@
 return {
-  { 'dietsche/vim-lastplace', event = { 'BufRead' } },
-  { 'godlygeek/tabular',      cmd = 'Tabularize' },
-  { 'kopischke/vim-fetch' },
-  {
-    'mg979/vim-visual-multi',
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  },
-  { 'tpope/vim-abolish',            keys = { "crs", "crm", "crc", "cru", "cr-", "cr." } },
-  {
-    'tpope/vim-bundler',
-    cmd = 'Bundler',
-    keys = { { "gf" } },
-    ft = 'ruby'
-  },
-  { 'tpope/vim-fugitive',           cmd = { 'Git', 'Gread', 'Gwrite', 'Glgrep', 'Gdiffsplit', 'Gsplit', 'Gedit' } },
-  { 'tpope/vim-repeat',             event = "VeryLazy" },
-  { 'editorconfig/editorconfig-vim' },
-  { 'kyazdani42/nvim-web-devicons' },
-  { 'skywind3000/asyncrun.vim',     event = "VeryLazy" },
-  { 'andymass/vim-matchup',         ft = { "javascriptreact", "ruby", "javascript", "json", "lua", "eruby" } },
-  {
-    'aca/marp.nvim',
-    ft = { 'md', 'markdown' }
-  },
-  {
-    'rorymckinley/vim-rubyhash',
-    ft = { 'erb', 'ruby' }
-  },
   {
     'AndrewRadev/tagalong.vim',
     ft = { 'javascriptreact', 'html', 'eruby', 'haml', }
@@ -37,16 +9,39 @@ return {
     event = { "BufRead", "BufEnter" }
   },
   {
+    'aca/marp.nvim',
+    ft = { 'md', 'markdown' }
+  },
+  {
+    'andymass/vim-matchup',
+    ft = { "javascriptreact", "ruby", "javascript", "json", "lua", "eruby" }
+  },
+  { 'dietsche/vim-lastplace',       event = { 'BufRead' } },
+  { 'editorconfig/editorconfig-vim' },
+  { 'godlygeek/tabular',            cmd = 'Tabularize' },
+  { 'kopischke/vim-fetch' },
+  { 'kyazdani42/nvim-web-devicons' },
+  {
+    'mg979/vim-visual-multi',
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+  },
+  { 'nvim-lua/popup.nvim',       dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'rorymckinley/vim-rubyhash', ft = { 'erb', 'ruby' } },
+  { 'shmup/vim-sql-syntax',      ft = 'sql' },
+  { 'skywind3000/asyncrun.vim',  event = "VeryLazy" },
+  {
+    'tpope/vim-abolish',
+    keys = { "crs", "crm", "crc", "cru", "cr-", "cr." }
+  },
+  { 'tpope/vim-bundler', cmd = 'Bundler',   keys = { { "gf" } }, ft = 'ruby' },
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'Gread', 'Gwrite', 'Glgrep', 'Gdiffsplit', 'Gsplit', 'Gedit' }
+  },
+  { 'tpope/vim-repeat',  event = "VeryLazy" },
+  {
     'windwp/nvim-ts-autotag',
     dependencies = { 'nvim-ts-context-commentstring' },
-    event = { "BufRead", "BufNewFile", "BufEnter" },
-  },
-  {
-    'nvim-lua/popup.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    'shmup/vim-sql-syntax',
-    ft = 'sql'
+    event = { "BufRead", "BufNewFile", "BufEnter" }
   }
 }
