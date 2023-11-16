76f77bf25c6d7734650c47876700a26e3e73a408
alexesba <alexesba@gmail.com>
2023-11-16 16:10:48 -0600

    Remove function for italic fonts

--- lua/alexesba/core/augroups/init.lua
+++ lua/alexesba/core/augroups/init.lua
@@ -1,4 +1,3 @@
 require('alexesba.core.augroups.column-limit')
 require('alexesba.core.augroups.file-types')
-require('alexesba.core.augroups.reload-colorscheme')
 require('alexesba.core.augroups.reload-vimrc')
