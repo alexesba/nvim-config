18212124d01869bd84953720cfc392b5ac93e146
alexesba <alexesba@gmail.com>
2023-11-16 18:11:12 -0600

    Ipdate reload function

--- lua/alexesba/core/augroups/reload-vimrc.lua
+++ lua/alexesba/core/augroups/reload-vimrc.lua
@@ -7,7 +7,6 @@ vim.api.nvim_create_autocmd(
   'BufWritePost', {
     pattern = {
       '*/.config/nvim/**/*.lua',
-      '*/.config/nvim/vim-files/plugins.vim',
       '*/.vimrc',
     },
     command = 'source $MYVIMRC | source %',
